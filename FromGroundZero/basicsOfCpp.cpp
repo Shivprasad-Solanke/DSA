0b8e0ee866371e993609cdcb056da6ce906574aa
Shivprasad-Solanke <shivsolanke12@gmail.com>
2026-08-24 02:37:30 +0530

    progressing with basics

---
+++ FromGroundZero/basicsOfCpp.cpp
@@ -0,0 +1,119 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // // Date types and Variables
+    // int someData = 10;
+    // float someFraction = 10.55;
+    // double someBigFraction = 204930.49302;
+    // char singleString = 'd';
+    // char someArray[10] = {'a','b','c','d','e','f','g'};\
+    // bool someBoolean = false;
+
+    // int inputtedNo1;
+    // int inputtedNo2;
+
+    // //Output statements
+    // cout << "this is an output statement" << endl;
+
+    // cout << "type first number" << endl;
+
+    // //Input statements
+    // cin >> inputtedNo1;
+    // cout << "type second number" << endl;
+    // cin >> inputtedNo2;
+    
+    // //Operators
+    // int square;
+    // square = inputtedNo1 * inputtedNo1;
+
+    // cout << "sum = " << inputtedNo1 + inputtedNo2 << endl << "sub = " << inputtedNo1 - inputtedNo2 << endl << "mul = " << inputtedNo1 * inputtedNo2 << endl << "div = " << inputtedNo1 / inputtedNo2 << endl << "modulo (rem) after div = " << inputtedNo1 % inputtedNo2 << endl;
+
+    // cout << "square of " << inputtedNo1 << " = " << square << endl;
+
+    // //Conditionals 
+    // //if
+    // int age;
+    // cout << "enter your age to get comments : " << endl;
+    // cin >> age;
+    // if (age < 13) 
+    //     cout << "your are good child!!" << endl;
+    // if (age >=13 && age < 18)
+    //     cout << "your are in your mischief teenage!!" << endl;
+    // if (age >=18) 
+    //     cout << "welcome to the adulthood!!" << endl;
+    
+    // //else if
+    // if (age < 13) 
+    //     cout << "your are good child!!" << endl;
+    // else if (age < 18)
+    //     cout << "your are in your mischief teenage!!" << endl;
+    // else if (age >=18) 
+    //     cout << "welcome to the adulthood!!" << endl;
+
+    // // else
+    // if (age < 13) 
+    //     cout << "your are good child!!" << endl;
+    // else if (age < 18)
+    //     cout << "your are in your mischief teenage!!" << endl;
+    // else 
+    //     cout << "welcome to the adulthood!!" << endl;
+
+    //switch        
+    // cout << "enter month number to get number of days in that month" << endl ;
+    // int x;
+    // cin >> x;
+    // switch (x) {
+    //     case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+    //         cout << "31 days" << endl;
+    //         break;
+    //     case 4: case 6: case 9: case 11:
+    //         cout << "30 days" << endl;
+    //         break;
+    //     case 2:
+    //         cout << "28 or 29 days" << endl;
+    //         break;
+    //     default :
+    //         cout << "enter correct value" << endl;
+    //         break;
+    // }
+
+    // Loops
+    //for loop
+    cout << "enter the charecter that you want to make pattern with" << endl;
+    char patternChar;
+    cin >> patternChar;
+    cout << "enter no of rows and no of columns : " << endl;
+    int cols,rows;
+    cin >> rows;
+    cin >> cols;
+    
+    for (int i = 1; i <= rows ; i++) {
+        for (int j = 1; j <= i ; j++) {
+            cout << patternChar;
+        }
+        cout << endl;
+    } 
+
+    //while loop
+    int i = rows;
+    //cout << "i out of while  = " << i << endl;
+
+    while(i >= 1) {
+        //cout << "i inside first while  = " << i << endl;
+        int j = i;
+        //cout << "j out of while  = " << j << endl;
+        while ( j >= 1) {
+            cout << patternChar;
+            j = --j;
+        //cout << "j inside while after operation  = " << j << endl;
+        }
+        cout << endl;
+        i = --i;
+    }
+
+
+return 0;
+
+}
